81e20111e3540bd9dc30c7ccd941feb3c29c0775
Abu Shahed Noyon <110329293+noyongit@users.noreply.github.com>
2022-08-27 11:54:58 +0600

    Add files via upload

---
+++ 1070.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int X,i=1;
+    scanf("%d", &X);
+    while(i<=12)
+    {
+        if(X%2==0)
+        {
+
+        }
+        else
+        {
+            printf("%d\n", X);
+        }
+        X++;
+        i++;
+    }
+    return 0;
+}
